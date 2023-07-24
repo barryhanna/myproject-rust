af97ae9a685a3cbba6520380bee41bf829095634
Barry Hanna <barryahanna@gmail.com>
2023-07-24 22:30:16 +0100

    Data types

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 fn main() {
-    let x = 5;
-    let mut y = 8;
+    let x: u8 = 2;
+    let mut y: u8 = 8;
+    let scores = (1, 'c', true);
+    let arr = [23, 34, 56];
+    let sli = &arr[0..1];
     y += 1;
     println!("Hello, world! {} {}", x, y);
+    println!("{}", x << 1);
+    println!("{}", scores.0);
+    println!("{}", scores.2);
+    println!("{:?}", arr);
+    println!("{:?}", sli);
 }
